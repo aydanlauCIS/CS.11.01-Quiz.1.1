dfa6df811ed2495fdda72a3590e2e9999c1ef55b
Aydan Lau <aydanlau@gmail.com>
2024-08-30 12:05:51 +0800

    Completed quiz

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,34 @@ import java.util.Scanner;
 *****************************************************************************************************
 * By writing your name in the space provided below, you are honouring the CIS academic honesty pledge 
 * “I have neither given nor received unauthorized aid on this piece of work.”
-* NAME: 
+* NAME: Aydan Lau
 *****************************************************************************************************
 */
 
 public class Main {
-
     public static void main(String[] args) {
+        // Declaring variables
+        Scanner myScanner = new Scanner(System.in);
+        int age;
+        String firstName;
+        String favouriteFood;
 
+        // Taking input for age
+        System.out.println("Enter your age:");
+        age = myScanner.nextInt();
 
+        // Taking input for first name
+        System.out.println("Enter your first name:");
+        myScanner.nextLine();
+        firstName = myScanner.nextLine();
 
+        // Taking input for favourite food
+        System.out.println("Enter your favourite food:");
+        favouriteFood = myScanner.nextLine();
 
+        // Output data collected
+        System.out.println("First name: " + firstName);
+        System.out.println("Age: " + age);
+        System.out.println("Favourite food: " + favouriteFood);
     }
-
 }
